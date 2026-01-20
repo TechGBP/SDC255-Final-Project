9fbe1f6a5a5df85e7a732441837577de4e1ad3b4
TechGBP <249761316+TechGBP@users.noreply.github.com>
2026-01-20 12:57:12 -0500

    Add files via upload

---
+++ 5-5 Final Project Code.cpp
@@ -0,0 +1,14 @@
+/*	 Weis, E. Balanta, G., and Varney, J.
+5.5 Coding Your Final Project in C
+01-20-2026	*/
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	
+	
+	
+	
+	return 0; ///ends main
+}
